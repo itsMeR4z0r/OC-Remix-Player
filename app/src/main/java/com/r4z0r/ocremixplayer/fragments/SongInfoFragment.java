633dbd4210b8d34fa8e0f6627c927125302708ad
Isaac Pinheiro <isaac.pinheiro@novavia.net.br>
2024-11-08 16:57:13 -0300

    Wip - song info

--- app/src/main/java/com/r4z0r/ocremixplayer/fragments/SongInfoFragment.java
+++ app/src/main/java/com/r4z0r/ocremixplayer/fragments/SongInfoFragment.java
@@ -25,17 +25,14 @@ import org.r4z0r.models.SongItem;
 public class SongInfoFragment extends Fragment {
     private SongInfoFragmentBinding binding;
 
-    private ExtendedFloatingActionButton fabOpcoes;
+    private ExtendedFloatingActionButton fabOpcoes, fabFavoritar, fabPlaylist, fabBaixar, fabCompartilhar;
     private RelativeLayout layoutOpcoes;
 
     private Musica musica;
     private ScrollView scrollView;
 
     @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = SongInfoFragmentBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
@@ -43,10 +40,25 @@ public class SongInfoFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         fabOpcoes = binding.songInfoFabOpcoes;
         layoutOpcoes = view.findViewById(R.id.song_info_rl_opcoes);
+
+        fabFavoritar = view.findViewById(R.id.song_info_fab_opcoes_favoritar);
+        fabPlaylist = view.findViewById(R.id.song_info_fab_opcoes_add_playlist);
+        fabBaixar = view.findViewById(R.id.song_info_fab_opcoes_baixar);
+        fabCompartilhar = view.findViewById(R.id.song_info_fab_opcoes_compartilhar);
+
+        fabFavoritar.setTranslationY(fabFavoritar.getHeight());
+        fabPlaylist.setTranslationY(fabPlaylist.getHeight());
+        fabBaixar.setTranslationY(fabBaixar.getHeight());
+        fabCompartilhar.setTranslationY(fabCompartilhar.getHeight());
+        layoutOpcoes.setTranslationY(layoutOpcoes.getHeight());
+        fabFavoritar.setAlpha(0);
+        fabPlaylist.setAlpha(0);
+        fabBaixar.setAlpha(0);
+        fabCompartilhar.setAlpha(0);
+        layoutOpcoes.setAlpha(0);
+
         scrollView = binding.songInfoScrollView;
-        if (OCRemixPlayerApplication.mInstance.getGlobal().getBundle() != null
-                && OCRemixPlayerApplication.mInstance.getGlobal().getBundle().containsKey("musica")
-                && OCRemixPlayerApplication.mInstance.getGlobal().getBundle().getSerializable("musica") != null) {
+        if (OCRemixPlayerApplication.mInstance.getGlobal().getBundle() != null && OCRemixPlayerApplication.mInstance.getGlobal().getBundle().containsKey("musica") && OCRemixPlayerApplication.mInstance.getGlobal().getBundle().getSerializable("musica") != null) {
             musica = (Musica) OCRemixPlayerApplication.mInstance.getGlobal().getBundle().getSerializable("musica");
         } else {
             OCRemixPlayerApplication.mInstance.getGlobal().getNavController().popBackStack();
@@ -79,21 +91,86 @@ public class SongInfoFragment extends Fragment {
 
     private void showOpcoes(boolean show) {
         if (!show) {
-            layoutOpcoes.animate()
-                    .translationY(layoutOpcoes.getHeight())
-                    .alpha(0)
-                    .setListener(new Animator.AnimatorListener() {
+            fabCompartilhar.animate().setDuration(150).alpha(0).translationY(fabCompartilhar.getHeight()).setListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(@NonNull Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(@NonNull Animator animator) {
+                    fabBaixar.animate().setDuration(150).alpha(0).translationY(fabBaixar.getHeight()).setListener(new Animator.AnimatorListener() {
                         @Override
                         public void onAnimationStart(@NonNull Animator animator) {
-                            layoutOpcoes.setVisibility(View.VISIBLE);
-                            scrollView.setEnabled(true);
-                            scrollView.animate().alpha(1F);
+
                         }
 
                         @Override
                         public void onAnimationEnd(@NonNull Animator animator) {
-                            layoutOpcoes.setVisibility(View.GONE);
-                            fabOpcoes.setIcon(getResources().getDrawable(R.drawable.baseline_keyboard_arrow_up_24, getContext().getTheme()));
+                            fabPlaylist.animate().setDuration(150).alpha(0).translationY(fabPlaylist.getHeight()).setListener(new Animator.AnimatorListener() {
+                                @Override
+                                public void onAnimationStart(@NonNull Animator animator) {
+
+                                }
+
+                                @Override
+                                public void onAnimationEnd(@NonNull Animator animator) {
+                                    fabFavoritar.animate().setDuration(150).alpha(0).translationY(fabFavoritar.getHeight()).setListener(new Animator.AnimatorListener() {
+                                        @Override
+                                        public void onAnimationStart(@NonNull Animator animator) {
+
+                                        }
+
+                                        @Override
+                                        public void onAnimationEnd(@NonNull Animator animator) {
+                                            layoutOpcoes.animate().setDuration(150).translationY(layoutOpcoes.getHeight()).alpha(0).setListener(new Animator.AnimatorListener() {
+                                                @Override
+                                                public void onAnimationStart(@NonNull Animator animator) {
+                                                    layoutOpcoes.setVisibility(View.VISIBLE);
+                                                    scrollView.setEnabled(true);
+                                                    scrollView.animate().alpha(1F);
+                                                }
+
+                                                @Override
+                                                public void onAnimationEnd(@NonNull Animator animator) {
+                                                    layoutOpcoes.setVisibility(View.GONE);
+                                                    fabOpcoes.setIcon(getResources().getDrawable(R.drawable.baseline_keyboard_arrow_up_24, getContext().getTheme()));
+                                                }
+
+                                                @Override
+                                                public void onAnimationCancel(@NonNull Animator animator) {
+
+                                                }
+
+                                                @Override
+                                                public void onAnimationRepeat(@NonNull Animator animator) {
+
+                                                }
+                                            });
+                                        }
+
+                                        @Override
+                                        public void onAnimationCancel(@NonNull Animator animator) {
+
+                                        }
+
+                                        @Override
+                                        public void onAnimationRepeat(@NonNull Animator animator) {
+
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onAnimationCancel(@NonNull Animator animator) {
+
+                                }
+
+                                @Override
+                                public void onAnimationRepeat(@NonNull Animator animator) {
+
+                                }
+                            });
                         }
 
                         @Override
@@ -106,21 +183,99 @@ public class SongInfoFragment extends Fragment {
 
                         }
                     });
+                }
+
+                @Override
+                public void onAnimationCancel(@NonNull Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(@NonNull Animator animator) {
+
+                }
+            });
         } else {
-            layoutOpcoes.animate()
-                    .translationY(0)
-                    .alpha(1)
-                    .setListener(new Animator.AnimatorListener() {
+            layoutOpcoes.animate().setDuration(150).translationY(0).alpha(1).setListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(@NonNull Animator animator) {
+                    layoutOpcoes.setVisibility(View.VISIBLE);
+                    scrollView.setEnabled(false);
+                    scrollView.animate().alpha(0.2F);
+                }
+
+                @Override
+                public void onAnimationEnd(@NonNull Animator animator) {
+                    fabOpcoes.setIcon(getResources().getDrawable(R.drawable.baseline_keyboard_arrow_down_24, getContext().getTheme()));
+                    fabFavoritar.animate().setDuration(150).translationY(0).alpha(1).setListener(new Animator.AnimatorListener() {
                         @Override
                         public void onAnimationStart(@NonNull Animator animator) {
-                            layoutOpcoes.setVisibility(View.VISIBLE);
-                            scrollView.setEnabled(false);
-                            scrollView.animate().alpha(0.2F);
+
                         }
 
                         @Override
                         public void onAnimationEnd(@NonNull Animator animator) {
-                            fabOpcoes.setIcon(getResources().getDrawable(R.drawable.baseline_keyboard_arrow_down_24, getContext().getTheme()));
+                            fabPlaylist.animate().setDuration(150).translationY(0).alpha(1).setListener(new Animator.AnimatorListener() {
+                                @Override
+                                public void onAnimationStart(@NonNull Animator animator) {
+
+                                }
+
+                                @Override
+                                public void onAnimationEnd(@NonNull Animator animator) {
+                                    fabBaixar.animate().setDuration(150).translationY(0).alpha(1).setListener(new Animator.AnimatorListener() {
+                                        @Override
+                                        public void onAnimationStart(@NonNull Animator animator) {
+
+                                        }
+
+                                        @Override
+                                        public void onAnimationEnd(@NonNull Animator animator) {
+                                            fabCompartilhar.animate().setDuration(150).translationY(0).alpha(1).setListener(new Animator.AnimatorListener() {
+                                                @Override
+                                                public void onAnimationStart(@NonNull Animator animator) {
+
+                                                }
+
+                                                @Override
+                                                public void onAnimationEnd(@NonNull Animator animator) {
+
+                                                }
+
+                                                @Override
+                                                public void onAnimationCancel(@NonNull Animator animator) {
+
+                                                }
+
+                                                @Override
+                                                public void onAnimationRepeat(@NonNull Animator animator) {
+
+                                                }
+                                            });
+                                        }
+
+                                        @Override
+                                        public void onAnimationCancel(@NonNull Animator animator) {
+
+                                        }
+
+                                        @Override
+                                        public void onAnimationRepeat(@NonNull Animator animator) {
+
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onAnimationCancel(@NonNull Animator animator) {
+
+                                }
+
+                                @Override
+                                public void onAnimationRepeat(@NonNull Animator animator) {
+
+                                }
+                            });
                         }
 
                         @Override
@@ -133,6 +288,18 @@ public class SongInfoFragment extends Fragment {
 
                         }
                     });
+                }
+
+                @Override
+                public void onAnimationCancel(@NonNull Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(@NonNull Animator animator) {
+
+                }
+            });
         }
     }
 }
